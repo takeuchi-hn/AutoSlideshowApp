13094ff1b52e818e9322354517c9c7514d1fb555
takeuchi-hn <sinnichi406t.t@gmail.com>
2021-10-16 01:13:31 +0900

    完成

--- app/src/main/java/jp/techacademy/taichi/takeuchi/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/taichi/takeuchi/autoslideshowapp/MainActivity.kt
@@ -1,48 +1,149 @@
 package jp.techacademy.taichi.takeuchi.autoslideshowapp
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.provider.MediaStore
+import android.content.ContentUris
+import android.database.Cursor
+import android.net.Uri
+import android.widget.CursorAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    //メンバー変数定義
+    var mCursor : Cursor? = null
+    var mFieldIndex : Int? = null
+    var mId : Long? = null
+    var mImageUri : Uri? = null
     private var mTimer: Timer? = null
-
-    // タイマー用の時間のための変数
-    private var mTimerSec = 0.0
-
     private var mHandler = Handler()
+
+    fun getImageUri(){
+        mFieldIndex = mCursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+        mId = mCursor!!.getLong(mFieldIndex!!)
+        mImageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, mId!!)
+        imageView.setImageURI(mImageUri)
+    }
+    private val PERMISSIONS_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        start_button.setOnClickListener {
-            if (mTimer == null){
-                mTimer = Timer()
-                mTimer!!.schedule(object : TimerTask() {
-                    override fun run() {
-                        mTimerSec += 0.1
-                        mHandler.post {
-                            timer.text = String.format("%.1f", mTimerSec)
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo()
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_CODE
+                )//TODO 拒否されたら落ちない様にする
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo()
+        }
+
+        start_pause_button.setOnClickListener(){
+            if(start_pause_button.text == "再生"){
+                start_pause_button.text = "停止"
+                next_button.text =""
+                back_button.text =""
+                if (mTimer == null) {
+                    // タイマーの作成
+                                        Log.d("TIMER","XXXXXXXXXXXXXXXX")
+                    mTimer = Timer()
+                    // タイマーの始動
+                    mTimer!!.schedule(object : TimerTask() {
+                        override fun run() {
+                            mHandler.post {
+                                if (start_pause_button.text == "停止") {
+                                    if (mCursor!!.moveToNext()) {
+                                    } else {
+                                        mCursor!!.moveToFirst()
+                                    }
+                                    getImageUri()
+                                } else if(start_pause_button.text == "再生") {
+                                }
+                            }
                         }
-                    }
-                }, 100, 100) // 最初に始動させるまで100ミリ秒、ループの間隔を100ミリ秒 に設定
+                    }, 1000, 2000) // 最初に始動させるまで100ミリ秒、ループの間隔を100ミリ秒 に設定
+                }
+            }else{
+                if (mTimer != null){
+                    mTimer!!.cancel()
+                    mTimer = null
+                }
+                start_pause_button.text = "再生"
+                next_button.text ="進む"
+                back_button.text ="戻る"
             }
         }
 
-        pause_button.setOnClickListener {
-            if (mTimer != null){
-                mTimer!!.cancel()
-                mTimer = null
+
+
+        next_button.setOnClickListener() {
+            if(start_pause_button.text == "再生"){
+                if (mCursor!!.moveToNext()) {
+                } else {
+                    mCursor!!.moveToFirst()
+                }
+                getImageUri()
             }
         }
-
-        reset_button.setOnClickListener {
-            mTimerSec = 0.0
-            timer.text = String.format("%.1f", mTimerSec)
+        back_button.setOnClickListener() {
+            if(start_pause_button.text == "再生") {
+                if (mCursor!!.moveToPrevious()) {
+                } else {
+                    mCursor!!.moveToLast()
+                }
+                getImageUri()
+            }
         }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }
+        }
+    }
+
+    private fun getContentsInfo() {
+        // 画像の情報を取得する
+        val resolver = contentResolver
+        mCursor = resolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                null, // 項目（null = 全項目）
+                null, // フィルタ条件（null = フィルタなし）
+                null, // フィルタ用パラメータ
+                null // ソート (nullソートなし）
+        )
+        if (mCursor!!.moveToFirst()) {
+            // indexからIDを取得し、そのIDから画像のURIを取得する
+            getImageUri()
+            imageView.setImageURI(mImageUri)
+        }
+    }
+
+
+   override fun onDestroy(){
+        super.onDestroy()
+        mCursor!!.close()
+   }
 }
+//   END   //
